23f6dd84b40151318310aa6f6f693f972ac9e1e8
Aadarsha2059 <dhakalaadarshababu20590226@gmail.com>
2025-05-30 12:07:24 +0545

    updated

---
+++ android/app/src/main/kotlin/com/aadarsha/countercubit/countercubit/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.aadarsha.countercubit.countercubit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/app.dart
@@ -0,0 +1,20 @@
+
+import 'package:countercubit/arithmetic_cubit_view.dart';
+import 'package:countercubit/cubit/arithmetic_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter BLoC',
+      home: BlocProvider(
+        create: (context) => ArithmeticCubit(),
+        child:ArithmeticCubitView (),
+      ),
+    );
+  }
+}
